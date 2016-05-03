e7d89870551eb187abdf4d9a20042caba0b73da6
bandito122 <mastrolilli.bob@gmail.com>
2016-05-03 03:11:14 +0200

    new commit

---
+++ src/testapplic/TestApplic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapplic;
+
+/**
+ *
+ * @author bobmastrolilli
+ */
+public class TestApplic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("yooo");
+        // TODO code application logic here
+    }
+    
+}
